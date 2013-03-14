2c87564345d71721eff7daa5464e3157114c03f8
John-Olav Storvold <johnolav.storvold@gmail.com>
2013-03-14 14:02:13 +0100

    KTN-Client forming

--- CalenderApp/src/ktn/Client.java
+++ CalenderApp/src/ktn/Client.java
@@ -12,13 +12,25 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import ktn.RequestEnum;
+import ktn.RequestObjects;
+import ktn.SendObject;
+
+import baseClasses.Alarm;
+import baseClasses.Appointment;
 import baseClasses.Person;
+import baseClasses.Room;
 
 //TCP client
 public class Client {
 	
+	private Socket connection;
+	
 	private int port;
 	private String serverAddress;
+	
+	private ObjectOutputStream objectOutput;
+	private ObjectInputStream objectInput;
 	
 	
 	public Client(int port, String serverAddress) {
@@ -26,21 +38,57 @@ public class Client {
 		this.serverAddress = serverAddress;
 	}
 	
+	public void connect() {
+		this.startClient();
+	}
+	
+	public void close() {
+		
+	}
+	
+	// Login function
+	public void login(String username, String password) {
+		String[] keyword = {username,password};
+		RequestObjects reqObj = new RequestObjects(RequestEnum.LOGIN, keyword);
+	}
+	
+	// Send function
+	private void send(RequestObjects reqObj) {
+		try {
+			this.objectOutput.writeObject(reqObj);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client: Object couldn't be sent");
+		}
+	}
+	
+	// receive function
+	private Object receive() {
+		Object receivedObject = null;
+		try {
+			receivedObject = this.objectInput.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return receivedObject;
+	}
+	
 	public void startClient() {
-		Socket serverConnection = null;
 		try {
 			// Create a TCP connection to Server/Client
-			serverConnection = new Socket(InetAddress.getByName(this.serverAddress), this.port);
+			connection = new Socket(InetAddress.getByName(this.serverAddress), this.port);
 			// Connection established
 			System.out.println("Connected to server");
 			//Acquire the InputStream from Socket
-			InputStream serverInputStream = serverConnection.getInputStream();
+			InputStream serverInputStream = connection.getInputStream();
 			// Creating InputSteamReader from the InputStream
 			InputStreamReader inFromServer = new InputStreamReader(serverInputStream);
 			// Creating Buffer for serverInputStream
 			BufferedReader stringFromServer = new BufferedReader(inFromServer);
 			// Acquiring the OutputStream from the Socket
-			OutputStream serverOutputStream = serverConnection.getOutputStream();
+			OutputStream serverOutputStream = connection.getOutputStream();
 			// Creating a PrintWriter class to use as output channel into the socket
 			PrintWriter outToServer = new PrintWriter(serverOutputStream, true);
 			
@@ -50,7 +98,7 @@ public class Client {
 			// Thread for sending messages; Passing over BufferedReader(Input from user) and PrintWriter
 			new OutputClass(inFromUser, outToServer).start();
 			// Thread for receiving messages; Passing over BufferedReader(Input from server)
-			new InputClass(stringFromServer).start();
+			new InputClass(serverInputStream).start();
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -92,21 +140,35 @@ public class Client {
 		private ObjectInputStream objectIn;
 		
 		
-		InputClass(InputStream input) {
+		InputClass(InputStream serverInputStream) {
 			try {
-				this.objectIn = new ObjectInputStream(input);
+				this.objectIn = new ObjectInputStream(serverInputStream);
 			} catch (IOException e) {e.printStackTrace();
 			}
 		}
 		public void run() {
 			while(true) {
+				SendObject object;
 				try {
-					Object obj = this.objectIn.readObject();
-					Person person = (Person) obj;
-					
-					String response = stringFromServer.readLine();
-					System.out.println(response);
-				} catch (IOException e) {
+					object = (SendObject) this.objectIn.readObject();
+					switch(object.getSendType()) {
+					case LOGIN:
+						Boolean bol = (Boolean) object.getObject();
+						break;
+					case APPOINTMENT:
+						Appointment app = (Appointment) object.getObject();
+						break;
+					case PERSON:
+						Person person = (Person) object.getObject();
+						break;
+					case ALARM:
+						Alarm alarm = (Alarm) object.getObject();
+					case ROOM:
+						Room room = (Room) object.getObject();
+					default:
+						break;
+					}
+				} catch (IOException | ClassNotFoundException e) {
 					e.printStackTrace();
 				}
 			}
