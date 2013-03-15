e9cfe311b70e9de051746b46a47b98290c10473f
Henrik <Henrik@78.91.13.222>
2013-03-15 16:52:19 +0100

    hei, hanso

--- CalenderApp/src/ktn/Client.java
+++ CalenderApp/src/ktn/Client.java
@@ -24,7 +24,7 @@ import baseClasses.Notification;
 public class Client {
 	
 	private Socket connection;
-	private final static String SERVERIP = "78.91.62.42";
+	private final static String SERVERIP = "78.91.13.222";
 	private final static int SERVERPORT = 4004;
 	
 	private ObjectOutputStream objectOutput;
