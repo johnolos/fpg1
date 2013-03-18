f09826b6cafb0bd3b5fa33cb69674b311da9d9f9
Henrik <Henrik@78.91.13.75>
2013-03-18 14:07:56 +0100

    nhasdfjhaølbga

--- CalenderApp/src/ktn/Server.java
+++ CalenderApp/src/ktn/Server.java
@@ -22,7 +22,7 @@ import baseClasses.Room;
 import database.Database;
 
 public class Server {
-	private final static String SERVERIP = "78.91.10.70";
+	private final static String SERVERIP = "78.91.13.75";
 	private final static int SERVERPORT = 4004;
 	private Database database;
 
